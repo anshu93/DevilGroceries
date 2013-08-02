6012fe80d4bd9607d641d26efe7abb39b9f8bbd7
anshu93 <anshuman.prasad93@gmail.com>
2013-08-02 12:01:30 -0400

    implementing cart

---
+++ public/shopping/home/js/cart-dropdown.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+	$('#cart-id').on("click", function(){
+		$(".cart-dropdown").toggle("linear");
+	});
+
+	$('#close-button').on("click", function(){
+		$(".cart-dropdown").toggle("linear");
+	});
+});
