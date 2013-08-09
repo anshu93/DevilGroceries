726415c5c54b4eaec5c21fc838d40a0ba02e3c1a
anshu93 <anshuman.prasad93@gmail.com>
2013-08-09 01:37:36 -0400

    cleared the order and orderitem relation databases

--- app/models/orderitemrelation.rb
+++ app/models/orderitemrelation.rb
@@ -1,6 +1,6 @@
 class Orderitemrelation < ActiveRecord::Base
 	attr_accessible :order_id, :item_id, :quantity
-	#belongs_to :order
+	belongs_to :order
 	belongs_to :item
 	validates :order_id, :item_id, :quantity, presence: true
 end
