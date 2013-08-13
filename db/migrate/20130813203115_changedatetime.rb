fac9809d0ea9431d15f1e264b5ce9231372e2da7
anshu93 <anshuman.prasad93@gmail.com>
2013-08-13 17:50:48 -0400

    checkout page

--- db/migrate/20130813203115_changedatetime.rb
+++ db/migrate/20130813203115_changedatetime.rb
@@ -1,6 +1,6 @@
 class Changedatetime < ActiveRecord::Migration
   def change
-  	change_column :order, :time, :string
-  	change_column :order, :date, :string
+  	change_column :orders, :time, :string
+  	change_column :orders, :date, :string
   end
 end
