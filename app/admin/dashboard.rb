bfd6114041f997f2fac6bd34b0f286aaceaf0dcf
anshu93 <anshuman.prasad93@gmail.com>
2013-08-16 11:51:51 -0400

    date matchin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,9 +3,9 @@ ActiveAdmin.register_page "Dashboard" do
 		columns do
 			column do
 				panel "Recent Orders" do
-					table_for Order.where(cart_status: "confirmed") do
+					table_for Order.where(cart_status: "confirmed").where(date: Date.today.strftime("%m/%d/%Y")) do
 						column :user_id 
-						column :room
+						column :campus
 						column :cart_status
 						column :date
 					end
