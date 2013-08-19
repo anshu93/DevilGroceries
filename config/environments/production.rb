3426280a574c78cd853651616b5fdc65f6a03c2b
anshu93 <anshuman.prasad93@gmail.com>
2013-08-19 02:17:38 -0400

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,16 +85,16 @@ Gro::Application.configure do
     :access_key_id => 'AKIAJQWVECXV7QRAYIAA',
     :secret_access_key => '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'
   }
+  }
 
-  # config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'devilgroceries.com',
+    domain:               'groceriesondemand.com',
     user_name:            'anshuman.prasad93@gmail.com',
     password:             'arsenal0',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
-}
 end
