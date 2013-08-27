fbb0ae9308b8bf6e0c35bb63e62e6963c545d15c
anshu93 <anshuman.prasad93@gmail.com>
2013-08-27 12:51:57 +0200

    mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,13 @@ class UserMailer < ActionMailer::Base
   default from: "anshuman"
   def confirmation(order)
   	@order = order
+  	if @order.campus == "East"
+		@dorm = EastSchedule.where(:dorm => @order.building).first
+	elsif @order.campus == "West"
+		@dorm = WestSchedule.where(:dorm => @order.building).first
+	else 
+		@dorm = CentralSchedule.all.first
+	end
   	@list = @order.orderitemrelations
   	mail(:to => order.email, :subject => "Order Confirmation")
   end
